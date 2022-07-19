707eb07933a13b269452a4bf3ebaf678aca87d0a
Rakibul Huda <rakib10rr3@gmail.com>
2022-07-19 18:38:12 +0600

    Time: 334 ms (5.26%), Space: 35 MB (47.37%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -1,15 +1,13 @@
+import kotlin.math.max
+
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        val smallest = if (word1.length < word2.length) word1 else word2
-        val longest = if (word1 == smallest) word2 else word1
-
         val answer = java.lang.StringBuilder()
 
-        for(i in smallest.indices) {
-            answer.append(word1[i]).append(word2[i])
+        for(i in 0 until max(word1.length,word2.length)) {
+            if (i < word1.length) answer.append(word1[i])
+            if (i < word2.length) answer.append(word2[i])
         }
-        for (i in smallest.length until  longest.length)
-            answer.append(longest[i])
 
         return answer.toString()
     }
